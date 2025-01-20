cdade3ced735b1602721c992c070e7b1a74a06d8
Mohamed Mahmoud <112190828+Mhmd0Mhmod@users.noreply.github.com>
2025-01-20 23:21:20 +0200

    Update until the server and client interactions

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import logo from "@/public/logo.png"
 function Logo() {
   return (
-    <Link href="/public" className="flex items-center gap-4 z-10">
+    <Link href="/" className="flex items-center gap-4 z-10">
       {/*<img src="/logo.png" height="60" loading={"lazy"} width="60" alt="The Wild Oasis logo" />*/}
       {/*  <Image src={"/logo.png"} alt={"The Wild Oasis logo"} height={60} width={60}/>*/}
         <Image src={logo} quality={1} height={60} width={60} alt={"The Wild Oasis logo"} />
